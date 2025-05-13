deadebe1154e8452367a02beb61548903f0ef284
thesalmanx <thesalmanx@gmail.com>
2025-05-13 14:01:03 +0500

    usernames added

--- app/(root)/user/[id]/page.tsx
+++ app/(root)/user/[id]/page.tsx
@@ -38,7 +38,7 @@ const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
             @{user?.username}
           </p>
           <p className="text-30-extrabold mt-7 text-center">
-            @{user?.username}
+            @{user?.email}
           </p>
           <p className="mt-1 text-center text-14-normal">{user?.bio}</p>
         </div>
